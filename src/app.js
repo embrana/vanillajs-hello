6cf790e225f7f4e2f42231819a8d95f25afafb56
embrana <67813424+embrana@users.noreply.github.com>
2024-06-30 22:09:09 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -2,10 +2,34 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+function getRandomElement(arr) {
+  return arr[Math.floor(Math.random() * arr.length)];
+}
+
+function generateExcuse() {
+  let who = ["The dog", "My grandma", "The mailman", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "my phone", "the car"];
+  let when = [
+    "before the class",
+    "when I was sleeping",
+    "while I was exercising",
+    "during my lunch",
+    "while I was praying"
+  ];
+
+  let excuse = `${getRandomElement(who)} ${getRandomElement(
+    action
+  )} ${getRandomElement(what)} ${getRandomElement(when)}.`;
+  return excuse;
+}
 
 window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
+  document.getElementById("excuse").innerHTML = generateExcuse();
 };
+
+// window.onload = function() {
+//
+// };
